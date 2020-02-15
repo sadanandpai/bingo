6b3a980965dc52cfb21056052c8df84aec9b554a
Sadanand Pai <Sadanand.Pai@trelleborg.com>
2020-02-15 21:37:30 +0530

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,56 @@
+navigator.serviceWorker.register("/sw.js");
+
+userTurn = "X";
+var winner = "";
+
+window.addEventListener("click", evt => {
+  if (
+    evt.srcElement.className.includes("box") &&
+    evt.srcElement.textContent == "" &&
+    winner === ""
+  ) {
+    evt.srcElement.textContent = userTurn;
+    userTurn = userTurn == "X" ? "O" : "X";
+
+    checkWinnerAlgorithm();
+  }
+});
+
+function checkWinnerAlgorithm() {
+  let code = "";
+  for (let i = 0; i < 9; i = i + 3) {
+    code = document.getElementById("square" + i).textContent;
+    if (
+      code !== '' && code === document.getElementById("square" + (i + 1)).textContent &&
+      code === document.getElementById("square" + (i + 2)).textContent
+    ) {
+      winner = code;
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    code = document.getElementById("square" + i).textContent;
+    if (
+      code !== '' && code === document.getElementById("square" + (i + 3)).textContent &&
+      code === document.getElementById("square" + (i + 6)).textContent
+    ) {
+      winner = code;
+    }
+  }
+
+  code = document.getElementById("square0").textContent;
+  if (
+    code !== '' && code === document.getElementById("square4").textContent &&
+    code === document.getElementById("square8").textContent
+  ) {
+    winner = code;
+  }
+
+  code = document.getElementById("square2").textContent;
+  if (
+    code !== '' && code === document.getElementById("square4").textContent &&
+    code === document.getElementById("square6").textContent
+  ) {
+    winner = code;
+  }
+}
---
+++ sw.js
@@ -0,0 +1,42 @@
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open("first-app").then(cache => {
+      return cache.addAll([
+        "/index.html",
+        "/src/css/app.css",
+        "/src/js/app.js"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", evt => {
+  evt.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== "first-app") {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", evt => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
+
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.open('first-app').then(cache => {
+        return cache.match(evt.request).then(cache => {
+          return cache;
+        });
+      });
+    })
+  );
+});
